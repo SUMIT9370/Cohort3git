90b662a4413f8f9ddcf42ca0737b840bb26a2b25
SUMIT9370 <sumitpatil9370@gmail.com>
2025-07-24 06:08:54 +0000

    user login are created

--- week8/routs/users.js
+++ week8/routs/users.js
@@ -2,6 +2,8 @@ const router= require("express");
 const userRouter=router();
 const z= require("zod");
 const bcrypt=require("bcrypt");
+const jwt =require("jsonwebtoken");
+const  jwtsecret="this is user secret";
 
 
 
@@ -47,9 +49,59 @@ userRouter.post("/signup", async function(req, res){
 });
 
 userRouter.post("/signin",async function(req, res){
-    res.json({
-        message:" now you are at signin page"
+    //input validation
+    const requirezod= z.object({
+        email:z.string().email().max(100).min(10),
+        name : z.string().max(100).min(2),
+        pass: z.string().max(50).min(6)
+
     })
+    const bodycheck=requirezod.safeParse(req.body);
+    if (!bodycheck.success){
+        res.status(300).json({
+            message: "invalid input", error : bodycheck.error
+        })
+    }
+
+    const {email,pass}=req.body;
+
+  try{
+      const user=await userdb.findOne({
+        email: email
+    })
+
+    if (user){
+        const passverify= bcrypt.compare(user.pass,pass)
+        if (passverify){
+            try{
+                const token=jwt.sign({
+                    user:user.email
+                },jwtsecret);
+                res.status(200).json({
+                    message: "Your now logged in as student", verifytoken: token
+                })
+
+            }catch(e){
+                res.status(400).json({
+                    message:"unable to sign the jwt"
+                })
+            }
+        }else{
+            res.status(301).json({
+                message:"wrong password "
+            })
+        }
+    }else{
+        res.status(300).json({
+            message:`NO user found of this email ${req.body.email}`
+        })
+    }
+  }catch(e){
+    res.status(500).json({
+        message:"sorry our backed is down "
+    })
+  }
+
 })
 
 module.exports={ userRouter};
