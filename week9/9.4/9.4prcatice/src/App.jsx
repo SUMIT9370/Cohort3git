f1287a187e2659dabd3195c3a6d06e508e7b6f4d
SUMIT9370 <sumitpatil9370@gmail.com>
2025-08-09 05:18:52 +0000

    cards in React

--- week9/9.4/9.4prcatice/src/App.jsx
+++ week9/9.4/9.4prcatice/src/App.jsx
@@ -1,15 +1,36 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App =()=>{
+  return(
+    <div>
+        <CARD>
+          <h1>hi</h1>
+          This is some content useImperativeHandle
 
-  return (
-    
-    <div> hi </div>
+        </CARD>
+
+        <CARD>
+          <h1>
+            Second 
+
+          </h1>
+          this is second card component
+        </CARD>
+     
+    </div>
   )
 }
+
+const CARD =({children})=>{
+  return(
+    <div style={{border:"1px, solid #ccc",borderRadius:"5px",
+      padding:'20px', margin:'10px', boxShadow:'2px 2px 5px rgba(0,0,0,0.1)',
+    }}>
+    
+      {children}
+    </div>
+  )
+}
+
 
 export default App
